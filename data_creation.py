6471050c424f9081e8c262dfbaa7a9a3415f478f
Daniel George <120543571+datcuriousguy@users.noreply.github.com>
2025-05-17 22:15:44 +0530

    Update data_creation.py
    Sorry was bit sick. added employment status with somewhat realistic weights

--- data_creation.py
+++ data_creation.py
@@ -47,9 +47,15 @@ def generate_row():
     The average salary is clipped to stay realistic: minimum $10,000, maximum $200,000.
 
     for the loan amount, annual_income * dti_ratio estimates how much the person can afford to borrow.
+
+    for the employment status, we have a list ["Stable Job", "Unstable Job", "Unemployed", "Recently Hired", "Student"].
+    out of these items, we pick any one at random but with some bias in the weights, namely a bias towards
+    student and stable job, to try to match the constantly changing reality of the job market.
+    
     """
 
     credit_score = int(np.clip(np.random.normal(680, 50), 300, 850))
     dti_ratio = round(np.clip(np.random.normal(0.35, 0.1), 0.1, 0.9), 2)
     annual_income = int(np.clip(np.random.normal(60000, 20000), 10000, 200000))
     loan_amount = int(np.clip(np.random.normal(annual_income * dti_ratio, 5000), 1000, 75000))
+    employment_status = random.choices(employment_statuses, weights=employment_weights, k=1)[0]
